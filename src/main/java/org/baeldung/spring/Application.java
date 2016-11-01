9647cec7c9a6e649cd4723433e6b445cde5ff33a
kais <kais@DESKTOP-K8FBENV>
2016-11-01 10:40:21 -0400

    ttt

---
+++ src/main/java/org/baeldung/spring/Application.java
@@ -0,0 +1,26 @@
+package org.baeldung.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+    // @Override
+    // public void onStartup(ServletContext servletContext) throws ServletException {
+    // super.onStartup(servletContext);
+    // servletContext.addListener(new RequestContextListener());
+    // }
+}
